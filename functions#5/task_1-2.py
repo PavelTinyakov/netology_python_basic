a38ce1dcd096592956b5da3ac067a5891d748e2a
paveltinyakov <tinyakov.p.g@yandex.ru>
2022-05-24 00:09:57 +0300

    add task_1-2 version 2

--- functions#5/task_1-2.py
+++ functions#5/task_1-2.py
@@ -1,45 +1,39 @@
-def show_people(**kwargs):
-    documents_ = kwargs['documents']
+def show_people():
     num_document = input('Введите номер документа : ')
-    for document in documents_:
+    for document in documents:
         if document['number'] == num_document:
             return f'Документ c номером "{num_document}" принадлежит - {document["name"]}\n'
     return f'Документ c номером "{num_document}" отсутствует в каталоге\n'
 
 
-def show_shelf(**kwargs):
-    directories_ = kwargs['directories']
+def show_shelf():
     num_document = input('Введите номер документа : ')
-    for k, v in directories_.items():
-        if num_document in v:
-            return f'Документ с номером "{num_document}" находится на полке - {k}\n'
+    for directory, numbers in directories.items():
+        if num_document in numbers:
+            return f'Документ с номером "{num_document}" находится на полке - {directory}\n'
     return f'Документ c номером "{num_document}" отсутствует на полках\n'
 
 
-def show_list(**kwargs):
-    documents_ = kwargs['documents']
-    lst_documents = ''
-    for document in documents_:
-        lst_documents += f'{" ".join(list(document.values()))}\n'
-    return lst_documents
+def show_list():
+    lst_document = ''
+    for document in documents:
+        lst_document += f'{" ".join(list(document.values()))}\n'
+    return lst_document
 
 
-def add_document(**kwargs):
-    documents_ = kwargs['documents']
-    directories_ = kwargs['directories']
+def add_document():
     type_document = input('Введите тип документа : ')
     num_document = input('Введите номер документа : ')
-
-    for document in documents_:
+    for document in documents:
         if document['number'] == num_document:
             return f'Документ c номером "{num_document}" уже есть в каталоге\n'
-
     name = input('Введите имя человека : ')
+
     num_directory = input('Введите номер полки : ')
-    if num_directory not in directories_:
+    if num_directory not in directories:
         return f'Документ не был внесен в каталог, указанная полка "{num_directory}" отсутствует\n'
 
-    new_document = dict(zip(list(documents_[0].keys()), [type_document, num_document, name]))
+    new_document = dict(zip(list(documents[0].keys()), [type_document, num_document, name]))
     documents.append(new_document)
     directories[num_directory].append(num_document)
 
@@ -47,69 +41,68 @@ def add_document(**kwargs):
            f'Размещен на полке {num_directory}\n'
 
 
-def delete_document(**kwargs):
-    documents_ = kwargs['documents']
-    directories_ = kwargs['directories']
+def delete_document():
     num_document = input('Введите номер документа : ')
+
     del_doc_directories = False
-    del_doc_documents = False
-    for lst in directories_.values():
+    for lst in directories.values():
         if num_document in lst:
             lst.remove(num_document)
             del_doc_directories = True
-    for document in documents_:
+
+    del_doc_documents = False
+    for document in documents:
         if document['number'] == num_document:
             documents.remove(document)
             del_doc_documents = True
+
     if del_doc_directories and del_doc_documents:
-        return f'Документ с номером "{num_document}" удален из архива\n'
+        return f'Документ с номером "{num_document}" удален из каталога и полок\n'
     if del_doc_directories:
         return f'Документ с номером "{num_document}" удален с полки, отсутствует в каталоге\n'
-    return f'Документ c номером "{num_document}" отсутствует в архиве\n'
+    return f'Документ c номером "{num_document}" отсутствует в каталоге и на полках\n'
 
 
-def move_document(**kwargs):
-    directories_ = kwargs['directories']
+def move_document():
     num_document = input('Введите номер документа : ')
     new_num_directory = input('На какую полку необходимо переместить : ')
-    if new_num_directory not in directories_:
+    if new_num_directory not in directories:
         return f'Документ не был перемещен, полка "{new_num_directory}" отсутствует\n'
+
     del_doc_directories = False
-    for lst in directories_.values():
+    for lst in directories.values():
         if num_document in lst:
             lst.remove(num_document)
             del_doc_directories = True
+
     if not del_doc_directories:
         return f'Документ c номером "{num_document}" отсутствует на полках\n'
     directories[new_num_directory].append(num_document)
     return f'Документ номер "{num_document}" был перемещен на полку номер "{new_num_directory}"\n'
 
 
-def add_shelf(**kwargs):
-    directories_ = kwargs['directories']
+def add_shelf():
     new_num_directory = input('Введите номер новой полки [число] : ')
     if not new_num_directory.isdigit():
         return f'Новая полка не добавлена, введено не числовое значение "{new_num_directory}"\n'
-    if new_num_directory in directories_:
+    if new_num_directory in directories:
         return f'Полка с номером "{new_num_directory}" уже есть\n'
     directories[new_num_directory] = []
     return f'Полка с номером "{new_num_directory}" успешно добавлена\n'
 
 
-def show_help_list(**kwargs):
-    commands = kwargs['commands']
+def show_help_list():
     lst_commands = ''
-    for k, v in commands.items():
-        lst_commands += f'{k} - {v}\n'
+    for command, description in USER_COMMANDS.items():
+        lst_commands += f'{command} - {description}\n'
     return lst_commands
 
 
-def main_launch(**kwargs):
-    commands = kwargs['commands']
+def main_launcher():
     command = input('Введите команду, ["exit" - выход] : ')
     if command == 'exit':
         return
-    if command in commands:
+    if command in USER_COMMANDS:
         print({
             'p': show_people,
             's': show_shelf,
@@ -119,11 +112,11 @@ def main_launch(**kwargs):
             'm': move_document,
             'as': add_shelf,
             'h': show_help_list
-        }[command](documents=documents, directories=directories, commands=user_commands))
+        }[command]())
     else:
         print(f'Команда "{command}" отсутствует в списке команд,\n'
               f'воспользуйтесь командой "h" для вывода списка возможных команд\n')
-    main_launch(**kwargs)
+    main_launcher()
 
 
 documents = [
@@ -138,7 +131,7 @@ directories = {
     '3': []
 }
 
-user_commands = {
+USER_COMMANDS = {
     'p': 'выведет имя человека по номеру документа',
     's': 'выведет номер полки по номеру документа',
     'l': 'выведет список всех документов',
@@ -146,8 +139,9 @@ user_commands = {
     'd': 'удалит документ из каталога и с полки',
     'm': 'переместит документ с текущей полки на целевую',
     'as': 'добавит новую полку в перечень полок',
-    'h': 'выведет список возможных команд'
+    'h': 'выведет список возможных команд',
+    'exit': 'завершить программу'
 }
 
 if __name__ == '__main__':
-    main_launch(documents=documents, directories=directories, commands=user_commands)
+    main_launcher()
